d30ae76d7ebab22c98674fea7cd967cdb5b66df1
squarjpg <e1b23047@oit.ac.jp>
2025-10-12 23:30:50 +0900

    効果音など追加
    pngを適用しておいて

---
+++ Assets/Script/soundplayer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class soundplayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //何かがboxCollider(当たり判定)に入ったとき
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("hit");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject soundObject = GameObject.Find("MainSoundObject");
+            AudioSource audioSource = soundObject.GetComponent<AudioSource>();
+            audioSource.Stop();
+            GetComponent<AudioSource>().Play();//AudioSourceのコンポーネントを取得し、Playメソッドを実行する。
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("hit");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject soundObject = GameObject.Find("MainSoundObject");
+            AudioSource audioSource = soundObject.GetComponent<AudioSource>();
+            audioSource.Stop();
+            GetComponent<AudioSource>().Play();//AudioSourceのコンポーネントを取得し、Playメソッドを実行する。
+        }
+    }
+}
